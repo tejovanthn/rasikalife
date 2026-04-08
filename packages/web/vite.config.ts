4cb115afd267746a4ef9c38edd492ea58f7e7c9d
Tejovanth N <1077633+tejovanthn@users.noreply.github.com>
2026-04-09 00:35:37 +0530

    optimise bug

--- packages/web/vite.config.ts
+++ packages/web/vite.config.ts
@@ -5,14 +5,4 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
   plugins: [envOnlyMacros(), reactRouter(), tsconfigPaths()],
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          'vendor-table': ['@tanstack/react-table'],
-          'vendor-markdown': ['react-markdown'],
-        },
-      },
-    },
-  },
 });
